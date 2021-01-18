3644446299768e53b524264ba32f5b1302a6d2ea
dengpengju <dengpju@163.com>
2021-01-18 22:34:51 +0800

    bind

--- rabbitmq/Message.go
+++ rabbitmq/Message.go
@@ -10,12 +10,28 @@ func Send(queue string, message string) error {
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer channel.Close()
 	q, err := channel.QueueDeclare(queue, false, false,false,false,nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	return channel.Publish("", q.Name, false, false,
+	q2, err := channel.QueueDeclare(queue+"union", false, false,false,false,nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = channel.ExchangeDeclare("UserExchange", "direct",false,false, false, false, nil)
+	if err != nil {
+		return err
+	}
+	// bind
+	err = channel.QueueBind(q.Name, "userreg", "UserExchange", false, nil)
+	if err != nil {
+		return err
+	}
+	err = channel.QueueBind(q2.Name, "userreg", "UserExchange", false, nil)
+	if err != nil {
+		return err
+	}
+	return channel.Publish("UserExchange", "userreg", false, false,
 		amqp.Publishing{
 			ContentType:"text/plain",
 			Body:[]byte(message),
