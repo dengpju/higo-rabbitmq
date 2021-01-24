0478e6500bbb4fc9f3a78856e199cffec095a46e
dengpengju <dengpju@163.com>
2021-01-24 22:53:55 +0800

    更新

--- producer/producer.go
+++ producer/producer.go
@@ -9,38 +9,59 @@ import (
 	"time"
 )
 
+var messageId int
+
 func main() {
-	client := rabbitmq.New(rabbitmq.Host("192.168.42.131"))
+	client := rabbitmq.New(rabbitmq.Host("192.168.8.99"))
 	defer client.Close()
 
-	messageId := 1
+	messageId = 1
 
+	//http.HandleFunc("/producer", func(w http.ResponseWriter, r *http.Request) {
+	//	mq := rabbitmq.Mq(rabbitmq.NewQueues().Append(rabbitmq.Queue("usertest"),
+	//		/**rabbitmq.Queue("usertestuion")*/),
+	//		rabbitmq.Exchange("UserExchange", "direct"),
+	//		"userreg",
+	//	).Confirm(true).NotifyReturn()
+	//
+	//	rand.Seed(time.Now().Unix())
+	//	mq.Message(amqp.Publishing{
+	//		ContentType: "text/plain",
+	//		MessageId:   fmt.Sprintf("%d", messageId),
+	//		Body:        []byte(fmt.Sprintf("gggggg%d", rand.Intn(1000)+1)),
+	//	}).Send()
+	//	messageId += 1
+	//	log.Println("发送消息成功")
+	//
+	//	w.Header().Set("Content-type", "application/json")
+	//	w.Write([]byte(fmt.Sprintf(`{"code":"200","data":%d}`, messageId)))
+	//})
+	//log.Println("服务启动成功")
+	//log.Fatal(http.ListenAndServe(":8080", nil))
+
+	mq := rabbitmq.Mq().
+		DeclareExchange(rabbitmq.Exchange("UserExchange", "direct")).
+		DeclareBindQueue(rabbitmq.NewQueues().Append(rabbitmq.Queue("usertest"),
+			/**rabbitmq.Queue("usertestuion")*/), "userreg").
+		SetConfirm(true).NotifyReturn()
+	cmq := rabbitmq.Mq()
+	fmt.Printf("%p\n", mq)
+	fmt.Printf("%p\n", cmq)
 	for {
+		cmq.Channel = mq.Channel
+		cmq.Exchange = mq.Exchange
+		cmq.Key = mq.Key
+		cmq.SetConfirm(mq.Confirm).NotifyReturn()
+		fmt.Printf("%p\n", cmq)
 		rand.Seed(time.Now().Unix())
-		rabbitmq.Mq(rabbitmq.NewQueues().Append(rabbitmq.Queue("usertest"),
-			/**rabbitmq.Queue("usertestuion")*/),
-			rabbitmq.Exchange("UserExchange", "direct"),
-			"userreg",
-		).Message(amqp.Publishing{
+		cmq.Context(amqp.Publishing{
 			ContentType: "text/plain",
-			MessageId:   fmt.Sprintf("%d",messageId),
+			MessageId:   fmt.Sprintf("%d", messageId),
 			Body:        []byte(fmt.Sprintf("gggggg%d", rand.Intn(1000)+1)),
-		}).
-			Message(amqp.Publishing{
-				ContentType: "text/plain",
-				MessageId:   fmt.Sprintf("%d",messageId + 1),
-				Body:        []byte(fmt.Sprintf("hhhhhh%d", rand.Intn(1000)+1)),
-			}).
-			Message(amqp.Publishing{
-				ContentType: "text/plain",
-				MessageId:   fmt.Sprintf("%d",messageId + 2),
-				Body:        []byte(fmt.Sprintf("jjjjjj%d", rand.Intn(1000)+1)),
-			}).
-			Qos(1,0, false).
-			Send()
-		messageId += 3
+		}).Send()
+		messageId += 1
 		log.Println("发送消息成功")
-		time.Sleep(time.Second * 1)
+		time.Sleep(time.Second * 5)
 	}
 
 }
--- rabbitmq/Message.go
+++ rabbitmq/Message.go
@@ -1,15 +1,19 @@
 package rabbitmq
 
 import (
+	"fmt"
 	"github.com/streadway/amqp"
 	"log"
 )
 
-type mq struct {
-	channel  *amqp.Channel
-	exchange *exchange
-	key      string //路由键
-	message  []amqp.Publishing
+type MQ struct {
+	Channel       *amqp.Channel
+	Exchange      *exchange
+	Key           string //路由键
+	Confirm       bool
+	Message       amqp.Publishing
+	notifyConfirm chan amqp.Confirmation
+	notifyReturn  chan amqp.Return
 }
 
 type Queues []*queue
@@ -40,49 +44,100 @@ func Exchange(name string, kind string) *exchange {
 	return &exchange{Name: name, Kind: kind}
 }
 
-func Mq(queues Queues, exchange *exchange, key string) *mq {
+func Mq() *MQ {
 	channel, err := Conn.Channel()
 	if err != nil {
 		log.Fatal(err)
 	}
+	return &MQ{Channel: channel}
+}
+
+func (this *MQ) DeclareExchange(exchange *exchange) *MQ {
 	//声明交换机
-	err = channel.ExchangeDeclare(exchange.Name, exchange.Kind, false, false, false, false, nil)
+	err := this.Channel.ExchangeDeclare(exchange.Name, exchange.Kind, false, false, false, false, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	// 声明队列
+	this.Exchange = exchange
+	return this
+}
+
+func (this *MQ) DeclareBindQueue(queues Queues, key string) *MQ {
+	this.Key = key
 	for _, queue := range queues {
-		q, err := channel.QueueDeclare(queue.Name, false, false, false, false, nil)
+		// 声明队列
+		q, err := this.Channel.QueueDeclare(queue.Name, false, false, false, false, nil)
 		if err != nil {
 			log.Fatal(err)
 		}
 		// 绑定队列
-		err = channel.QueueBind(q.Name, key, exchange.Name, false, nil)
+		err = this.Channel.QueueBind(q.Name, this.Key, this.Exchange.Name, false, nil)
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
-	return &mq{channel: channel, exchange: exchange, key: key}
-}
-
-func (this *mq) Message(context amqp.Publishing) *mq {
-	this.message = append(this.message, context)
 	return this
 }
 
-func (this *mq) Qos(prefetchCount, prefetchSize int, global bool) *mq {
-	this.channel.Qos(prefetchCount, prefetchSize, global)
+func (this *MQ) Context(context amqp.Publishing) *MQ {
+	this.Message = context
 	return this
 }
 
-func (this *mq) Send() (err []error) {
-	defer this.channel.Close()
-	if len(this.message) > 0 {
-		for _,message :=range this.message {
-			err = append(err, this.channel.Publish(this.exchange.Name, this.key, false, false,
-				message,
-			))
-		}
+func (this *MQ) Qos(prefetchCount, prefetchSize int, global bool) *MQ {
+	err := this.Channel.Qos(prefetchCount, prefetchSize, global)
+	if err != nil {
+		log.Fatal(err)
 	}
+	return this
+}
+
+func (this *MQ) SetConfirm(b bool) *MQ {
+	err := this.Channel.Confirm(false)
+	if err != nil {
+		log.Fatal(err)
+	}
+	this.notifyConfirm = this.Channel.NotifyPublish(make(chan amqp.Confirmation))
+	this.Confirm = b
+	return this
+}
+
+func (this *MQ) listenConfirm() {
+	defer this.Channel.Close()
+	ret := <-this.notifyConfirm
+	if ret.Ack {
+		log.Println("Confirm消息发送成功")
+	} else {
+		log.Println("Confirm消息发送失败")
+	}
+}
+
+func (this *MQ) NotifyReturn() *MQ {
+	this.notifyReturn = this.Channel.NotifyReturn(make(chan amqp.Return))
+	go this.listenReturn()
+	return this
+}
+
+func (this *MQ) listenReturn() {
+	ret := <-this.notifyReturn
+	if string(ret.Body) != "" {
+		log.Println("消息没有正确入列:", string(ret.Body))
+	} else {
+		log.Println("消息正确入列:")
+	}
+}
+
+func (this *MQ) Send() (err error) {
+	// mandatory:
+	//如果为true,在exchange正常且可到达的情况下。如果exchange+routeKey无法投递给queue，那么MQ会将消息返还给生产者
+	//如果为false时，则直接丢弃
+	fmt.Println(this.Message)
+	err = this.Channel.Publish(this.Exchange.Name, this.Key, true, false,
+		this.Message,
+	)
+	if this.Confirm {
+		this.listenConfirm()
+	}
+
 	return
 }
